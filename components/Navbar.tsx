26cdcb2a806e4190756bc78d3683cab623e18de1
mhmAdmin1122 <hasaanad12min@gmail.com>
2023-05-18 19:08:14 +0500

    about page build for all set of view

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,78 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "./Images/android-chrome-192x192.png";
 import Link from "next/link";
 
 export default function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = (event: any) => {
+    setIsActive((current) => !current);
+  };
   return (
     <>
-      <nav className="bg-dark navbar-conatiner justify-content-between align-items-center py-2 m-0 px-2" style={{width: "100%"}}>
+      <nav
+        className="bg-dark navbar-conatiner justify-content-between align-items-center py-2 m-0 px-2"
+        style={{ width: "100%" }}
+      >
         <div className="logo-box d-flex w-10">
           <Link href="/">
             <Image src={logo} alt="logo" className="w-10" />
           </Link>
         </div>
         <ul className="navbar-tab-list d-flex align-items-end m-0">
-          <Link className="text-decoration-none" href="/"><li className="navbarTabs px-4 fs-6 fw-bold">Home</li></Link>
-          <Link className="text-decoration-none" href="/about"><li className="navbarTabs px-4 fs-6 fw-bold">About</li></Link>
-          <Link className="text-decoration-none" href="/services"><li className="navbarTabs px-4 fs-6 fw-bold">Services</li></Link>
-          <Link className="text-decoration-none" href="/excerquiz"><li className="navbarTabs px-4 fs-6 fw-bold">Excercise &amp; Quiz</li></Link>
-          <Link className="text-decoration-none" href="/product"><li className="navbarTabs px-4 fs-6 fw-bold">Products</li></Link>
-          <Link className="text-decoration-none" href="/tools"><li className="navbarTabs px-4 fs-6 fw-bold">Tools</li></Link>
+          <Link className="text-decoration-none" href="/">
+            <li className="navbarTabs px-4 fs-6 fw-bold">Home</li>
+          </Link>
+          <div
+            className="text-decoration-none nav-tabAbout position-relative"
+            onClick={handleClick}
+          >
+            <li className="navbarTabs px-4 fs-6 fw-bold">About</li>
+            {isActive && (
+              <div className="position-absolute aboutList">
+                <div className="about-TabList">
+                  <ul className="abouttabList">
+                    <Link href="/about">
+                      <li className="aboutListTabs">About Us</li>
+                    </Link>
+                    <Link href="/">
+                      <li className="aboutListTabs">Contact Us</li>
+                    </Link>
+                    <Link href="/">
+                      <li className="aboutListTabs">Disclaimer</li>
+                    </Link>
+                    <Link href="/">
+                      <li className="aboutListTabs">Privacy Policy</li>
+                    </Link>
+                    <Link href="/">
+                      <li className="aboutListTabs">Term & Services</li>
+                    </Link>
+                    <Link href="/">
+                      <li className="aboutListTabs">
+                        Copy Right Infringements
+                      </li>
+                    </Link>
+                  </ul>
+                </div>
+              </div>
+            )}
+          </div>
+          <Link className="text-decoration-none" href="/services">
+            <li className="navbarTabs px-4 fs-6 fw-bold">Services</li>
+          </Link>
+          <Link className="text-decoration-none" href="/excerquiz">
+            <li className="navbarTabs px-4 fs-6 fw-bold">
+              Excercise &amp; Quiz
+            </li>
+          </Link>
+          <Link className="text-decoration-none" href="/product">
+            <li className="navbarTabs px-4 fs-6 fw-bold">Products</li>
+          </Link>
+          <Link className="text-decoration-none" href="/tools">
+            <li className="navbarTabs px-4 fs-6 fw-bold">Tools</li>
+          </Link>
         </ul>
       </nav>
     </>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import Navbar from "@/../components/Navbar";
+import MobileHeader from "@/../components/MobileHeader";
+import Footer from "@/../components/Footer";
+import Image from "next/image";
+import aboutPic from '@/../public/images/about.jpg'
 
 export default function page() {
   return (
     <div>
-      pae
+      <Navbar />
+      <MobileHeader />
+      <div className="aboutUsPageContent-box">
+        <div className="welcomeLine-box">
+          <h1>Welcome to AIO!!!</h1>
+          <Image src={aboutPic} alt="Lwelcome-line-container-pic"/>
+        </div>
+        <div className="detailsAbot-about">
+          <p className="aboutDetail">
+            AIO is a Professional all Needs Platform. Here we will provide you
+            only interesting content, which you will like very much. We{"'"}re
+            dedicated to providing you the best of all Needs , with a focus on
+            dependability and Online Asset . We{"'"}re working to turn our
+            passion for all Needs into a booming online website. We hope you
+            enjoy our all Needs as much as we enjoy offering them to you. I will
+            keep posting more important posts on my Website for all of you.
+            Please give your support and love.
+          </p>
+          <b className="thank-Line">Thanks For Visiting Our Site</b>
+          <b className="niceDay-Line">Have a nice day!</b>
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
+  );
 }
